8f5b74cd1fe3b029db92270096c0ad775d832a1e
saudkalpana <saudkalpana476@gmail.com>
2023-03-31 15:53:11 +0545

    style:style button and texFfield

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,8 +2,6 @@
 import React,{useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TextField, Button } from '@mui/material';
-
-
 const Form = () => {
     const navigate=useNavigate();
     const [username, setUsername] = useState('');
